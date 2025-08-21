30d6f87a4c1faa09a794eec20b1f94515fca795f
kherhozen <kherhozen@protonmail.com>
2025-08-21 22:41:59 +0200

    add feedback

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 @app.route('/runled')
 def runled():
     subprocess.run(["python", "sources/Navio/Python/myi2c.py"])
-    return 'Yummy cakes!'
+    return 'led demo done'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
