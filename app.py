e3c1fd31ff2e3110facdd28f6d2e4178f94824b0
kherhozen <kherhozen@protonmail.com>
2025-08-27 22:40:46 +0200

    led status feedback

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, render_template, request, jsonify
 import subprocess
 
@@ -25,8 +27,10 @@ def confled():
         with open('/home/kherhozen/sources/Navio/Python/conf_led', 'w') as f:
             f.write("{},{},{},{}".format(received_data.get('mode'), received_data.get('red'),
                                          received_data.get('green'), received_data.get('blue')))
+        time.sleep(0.1)
+        with open('/home/kherhozen/sources/Navio/Python/conf_led_status', 'r') as f:
             return jsonify({
-                "message": "Data received successfully!"
+                "message": f.read()
             })
     else:
         return jsonify({"error": "Request body must be JSON"}), 400
